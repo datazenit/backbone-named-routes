df866ae8c13b9b050a0648adcc0c3173d36a71b2
Lauris <lauris@discuss.lv>
2014-03-13 15:34:31 +0200

    add: bower file

---
+++ bower.js
@@ -0,0 +1,9 @@
+{
+  "name": "datazenit/backbone-named-routes",
+  "version": "1.0.1",
+  "description": "Add named routes to your Backbone application.",
+  "license": "MIT",
+  "dependencies": {
+    "backbone": null
+  },
+}
